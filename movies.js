aa9e0f5fc2484ed722f87fdad5078ed8015745e9
chicacode <gerahernan.al@gmail.com>
2022-09-14 18:18:03 -0700

    feat: added movies

---
+++ movies.js
@@ -0,0 +1,32 @@
+let movies = [
+    {
+      "name": "Matrix",
+      "date_released": new Date('1999-31-03'),
+      "name_director": ["Lana Wachowski", "Lilly Wachowski"],
+      "key_actor": "Keanu Reaves"
+    },
+    {
+      "name": "Big Fish",
+      "date_released": new Date('2003-12-10'),
+      "name_director": "Tim Burton",
+      "key_actor": "Ewan McGregor"
+    },
+    {
+      "name": "Les Misérables",
+      "date_released": new Date('2012-25-12'),
+      "name_director": "Tom Hooper",
+      "key_actor": "Hugh Jackman"
+    },
+    {
+      "name": "Lost in Translation",
+      "date_released": new Date('2004-31-03'),
+      "name_director": "Sofia Coppola",
+      "key_actor": "Bill Murray"
+    },
+    {
+      "name": "Interstellar",
+      "date_released": new Date('2014-07-11'),
+      "name_director": "Christopher Nolan",
+      "key_actor": "Matthew McConaughey"
+    },
+  ]
